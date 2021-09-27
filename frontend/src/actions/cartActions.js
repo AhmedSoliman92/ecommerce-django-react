738631b893464a17163348a1607d2e5f24e3dcd8
AhmedSoliman92 <ahmed.soli101@gmail.com>
2021-09-27 06:41:25 +0200

    cart  functionality

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { CART_ADD_ITEM } from '../constants/cartConstants';
 
 export const addToCard = (id, qty) => async (dispatch, getstate) => {
-    const {data} = await axios.get(`${id}`)
+    const {data} = await axios.get(`http://127.0.0.1:8000/api/products/${id}`)
 
     dispatch({
         type : CART_ADD_ITEM,
--- frontend/src/screens/Cart.js
+++ frontend/src/screens/Cart.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import { useDispatch,useSelector } from 'react-redux'
+import { addToCard } from '../actions/cartActions'
+const Cart = ({match, location, history}) => {
+    const productId = match.params.id
+    const qty = location.search ? Number(location.search.split('=')[1]) : 1
+    
 
-const Cart = () => {
-    return (
+    const dispatch = useDispatch()
+
+    const cart = useSelector(state => state.cart)
+    const {cartItems} = cart
+    useEffect(()=>{
+        if (productId){
+            dispatch(addToCard(productId,qty))
+        }
+    },[dispatch,productId,qty])
+    return ( 
         <div>
             cart
         </div>
